c7d085a6364732776521af32f66f9e8e46e7b8ad
Vladchuvashov <71549752+Vladchuvashov@users.noreply.github.com>
2020-12-07 19:11:53 +0300

    Update simple_service.py

--- docker_compose/data_client/app/src/simple_service.py
+++ docker_compose/data_client/app/src/simple_service.py
@@ -1,7 +1,7 @@
 import os
 import socketserver
 from http import HTTPStatus
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse
 
 import psycopg2
 from msgpack import packb, unpackb
